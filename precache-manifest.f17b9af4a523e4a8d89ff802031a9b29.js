621b16dc02f5f468e0e81f290f773a769397561b
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-08-03 17:56:09 +0530

    Updates

---
+++ precache-manifest.f17b9af4a523e4a8d89ff802031a9b29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4d502fbe00f8c76f69ebc63b35f68b",
+    "url": "/react-sudoku-solver/index.html"
+  },
+  {
+    "revision": "29ac254fc1b7b9bec6e2",
+    "url": "/react-sudoku-solver/static/css/main.5267074f.chunk.css"
+  },
+  {
+    "revision": "f4395db77a7bc25cd33d",
+    "url": "/react-sudoku-solver/static/js/2.2c1b458c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sudoku-solver/static/js/2.2c1b458c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ac254fc1b7b9bec6e2",
+    "url": "/react-sudoku-solver/static/js/main.346b6a92.chunk.js"
+  },
+  {
+    "revision": "efc1e81a56317abe1b6f",
+    "url": "/react-sudoku-solver/static/js/runtime-main.b7e6ed08.js"
+  }
+]);
